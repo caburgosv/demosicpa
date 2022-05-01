508a9e179f9190569652cc15f6682d4fcff8b085
Carlos Burgos <cburgos@ec.krugercorp.com>
2022-05-01 09:40:31 -0500

    Creation base project angular to demosipca Git initial commit demosipca in https://app.clickup.com/t/2nw2hww

---
+++ src/test/java/ec/com/sipca/demosipca/DemosipcaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.sipca.demosipca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosipcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
